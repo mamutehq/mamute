90b5eb5e8a094476a114f7e0c9a9064e5a56468a
Cosmin Stroe <cosmin@mailbox.org>
2018-07-10 01:04:26 -0500

    Two migrations converted to Liquibase, NewsletterSentLog entity is properly configured.

--- src/main/java/org/mamute/model/NewsletterSentLog.java
+++ src/main/java/org/mamute/model/NewsletterSentLog.java
@@ -2,14 +2,16 @@ package org.mamute.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
-//@Entity
-
+@Entity
+@Table(name = "NewsletterSentLog")
 public class NewsletterSentLog {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private final LocalDateTime createdAt = LocalDateTime.now();
