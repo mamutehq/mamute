20f3b718d9a2d47b20f78dd69b55e595820ed9fe
Cosmin Stroe <cosmin@mailbox.org>
2018-07-12 06:03:41 -0500

    Use xmllint and xpath to get project version.

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -4,4 +4,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 POM=$SCRIPT_DIR/../pom.xml
 
-grep "version" $POM | head -n 1 | sed 's/[[:space:]]//g' | sed 's/<version>//' | sed 's/<\/version>//'
+hash xmllint 2>/dev/null || { echo >&2 "Command not found: xmllint.  Aborting."; exit 1; }
+
+xmllint --xpath "/*[namespace-uri()='http://maven.apache.org/POM/4.0.0' and local-name()='project']/*[namespace-uri()='http://maven.apache.org/POM/4.0.0' and local-name()='version']/text()" $POM
