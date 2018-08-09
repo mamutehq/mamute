360bfdff000bade974eb8c50851b61c396efdd50
Cosmin Stroe <cosmin@mailbox.org>
2018-08-08 21:30:57 -0500

    More migrations converted to Liquibase. (#39)
    * More migrations converted to Liquibase.
    
    * More migrations, enable the related entities.

--- src/main/java/org/mamute/model/Attachment.java
+++ src/main/java/org/mamute/model/Attachment.java
@@ -4,17 +4,18 @@ import br.com.caelum.vraptor.observer.upload.UploadedFile;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Transient;
 import java.awt.image.BufferedImage;
 import java.time.LocalDateTime;
 
-//@Entity
+@Entity
 public class Attachment {
 
-	@GeneratedValue
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Transient
