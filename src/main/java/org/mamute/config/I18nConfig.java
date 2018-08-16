ba913543235ebee5307dce5ce7e0fc49e326e013
Cosmin Stroe <cosmin.stroe@uptake.com>
2018-08-15 23:00:08 -0500

    More of the home page.

---
+++ src/main/java/org/mamute/config/I18nConfig.java
@@ -0,0 +1,42 @@
+package org.mamute.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+/**
+ * Reference: https://stackoverflow.com/questions/36531131/i18n-in-spring-boot-thymeleaf
+ */
+@Configuration
+public class I18nConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("mamute-messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
