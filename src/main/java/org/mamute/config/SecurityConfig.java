02deb84200a5c96a16d1ec961928507a7a65f2e9
Cosmin Stroe <cosmin@mailbox.org>
2018-09-25 07:54:52 -0500

    More templates (#56)
    * Initial header.
    
    * More of the home page.
    
    * Add more templates, get the front page looking ok.
    
    * Initial work on Question Form.
    
    * Working main page, and login page

---
+++ src/main/java/org/mamute/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package org.mamute.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // @formatter:off
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/ask")
+                    .hasRole("USER")
+                .and()
+                    .formLogin()
+                    .permitAll()
+                .and()
+                    .logout()
+                    .permitAll();
+    }
+    // @formatter:on
+
+    // @formatter:off
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser(User.withDefaultPasswordEncoder().username("user@email.com").password("password").roles("USER"));
+    }
+    // @formatter:on
+}
